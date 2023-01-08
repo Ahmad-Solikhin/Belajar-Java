094c4838d8021d40b378491f8500fea65e39464f
Ahmad Solikhin Gayuh Raharjo <ahmadsgr39@gmail.com>
2023-01-08 22:17:01 +0700

    Initiate Apache Maven

---
+++ ApacheMaven/ApacheMaven-app/src/main/java/maven/App.java
@@ -0,0 +1,20 @@
+package maven;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+
+        Person person = new Person("Ahmad Solikhin Gayuh Raharjo");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
