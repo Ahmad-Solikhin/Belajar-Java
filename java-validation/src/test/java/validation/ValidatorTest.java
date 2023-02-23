d913a4da49a6b46a5f123164badd15db4feb92ac
Ahmad Solikhin Gayuh Raharjo <ahmadsgr39@gmail.com>
2023-02-23 19:20:15 +0700

    Initiate java-validation

---
+++ java-validation/src/test/java/validation/ValidatorTest.java
@@ -0,0 +1,20 @@
+package validation;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest {
+
+    @Test
+    void createValidator(){
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+
+        Assertions.assertNotNull(validator);
+
+        validatorFactory.close();
+    }
+}
