755541b73ec2a7e9aff159648aba7f25a12cde25
Ahmad Solikhin Gayuh Raharjo <ahmadsgr39@gmail.com>
2023-05-09 16:41:50 +0700

    Initiate and finish Java CSV course

---
+++ Java-CSV/src/test/java/csv/JavaCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
